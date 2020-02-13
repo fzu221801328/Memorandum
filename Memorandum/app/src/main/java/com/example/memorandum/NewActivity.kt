b4657cb84d64c5d4a7bd1e44342befdaf4aa0074
fzu221801328 <962900904@qq.com>
2020-02-13 16:10:45 +0800

    实现点击闹钟（图标）出现日期时间选择器

--- Memorandum/app/src/main/java/com/example/memorandum/NewActivity.kt
+++ Memorandum/app/src/main/java/com/example/memorandum/NewActivity.kt
@@ -1,7 +1,6 @@
 package com.example.memorandum
 
-import android.app.Activity
-import android.app.AlertDialog
+import android.app.*
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -14,10 +13,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.note.*
 
 import kotlinx.android.synthetic.main.note_item.*
+import java.util.*
 
+@Suppress("DEPRECATION")
 class NewActivity: AppCompatActivity() {
 
+    var mYear = 0
+    var mMonth = 0
+    var mDay = 0
+    var mHour = 0
+    var mMinute = 0
 
+    val DATE_DIALOG = 1
+    val TIME_DIALOG = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +55,12 @@ class NewActivity: AppCompatActivity() {
 
 
 
+        val ca = Calendar.getInstance()
+        mYear = ca[Calendar.YEAR]
+        mMonth = ca[Calendar.MONTH]
+        mDay = ca[Calendar.DAY_OF_MONTH]
+        mHour = ca[Calendar.HOUR_OF_DAY]
+        mMinute = ca[Calendar.MINUTE]
 
     }
 
@@ -93,6 +107,12 @@ class NewActivity: AppCompatActivity() {
                     }).create().show()
 
             }
+            R.id.menu_alarm ->{
+                dateDisplay.visibility = View.VISIBLE
+                timeDisplay.visibility = View.VISIBLE
+                showDialog(TIME_DIALOG)
+                showDialog(DATE_DIALOG)
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -124,5 +144,44 @@ class NewActivity: AppCompatActivity() {
             finish()
         }
 
+    }
+
+
+    override fun onCreateDialog(id: Int): Dialog? {
+        when (id) {
+
+            DATE_DIALOG -> return DatePickerDialog(this,
+                DatePickerDialog.THEME_HOLO_LIGHT,mdateListener, mYear, mMonth, mDay)
+
+            TIME_DIALOG -> return TimePickerDialog(this,
+                TimePickerDialog.THEME_HOLO_LIGHT,mtimeListener,mHour,mMinute,false)
+            //DatePickerDialog(this, mdateListener, mYear, mMonth, mDay)
+        }
+        return null
+    }
+
+    /**
+     * 设置日期 利用StringBuffer追加
+     */
+    fun displayDate() {
+        dateDisplay!!.text = StringBuffer().append(mYear).append("-").append(mMonth + 1).append("-").append(mDay).append(" ")
+    }
+
+    fun displayTime(){
+        timeDisplay!!.text = StringBuffer().append(mHour).append(":").append(mMinute).append(" ")
+    }
+
+    private val mdateListener =
+        DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            mYear = year
+            mMonth = monthOfYear
+            mDay = dayOfMonth
+            displayDate()
+        }
+
+    private val mtimeListener = TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+        mHour = hourOfDay
+        mMinute = minute
+        displayTime()
     }
 }
